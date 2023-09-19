c5c7b054ab8ca12235ab171e818015d92266b91d
valeriia hashemi <111574869+feroztia@users.noreply.github.com>
2023-09-19 13:39:06 +0200

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-"use strict";
-const openMenu = document.querySelector("#open-menu");
-const ul = document.querySelector("nav > ul");
-const closeMenu = document.querySelector("#close-menu");
-console.log(ul);
+const openMenuButton = document.getElementById("open-menu");
+const closeMenuButton = document.getElementById("close-menu");
+const nav = document.querySelector("nav");
+const overlay = document.querySelector(".overlay");
 
-openMenu.addEventListener("click", () => {
-  console.log("I'm being clicked");
-  ul.classList.toggle("show");
-  closeMenu.style.display = "block";
-  openMenu.style.display = "none";
+openMenuButton.addEventListener("click", () => {
+    nav.classList.add("active");
+    overlay.classList.add("active");
 });
 
-closeMenu.addEventListener("click", () => {
-  closeMenu.style.display = "none";
-  openMenu.style.display = "block";
-  ul.classList.toggle("show");
+closeMenuButton.addEventListener("click", () => {
+    nav.classList.remove("active");
+    overlay.classList.remove("active");
+});
+
+overlay.addEventListener("click", () => {
+    nav.classList.remove("active");
+    overlay.classList.remove("active");
 });
